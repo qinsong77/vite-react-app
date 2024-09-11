2513de0af94fc1814e1c546a492c48e2e53e6504
notend <xyqinsong@163.com>
2024-09-11 16:04:01 +0800

    style: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -10,7 +11,8 @@ export default tseslint.config(
   {
     files: ['**/*.{ts,tsx}'],
     settings: { react: { version: '18.3' } },
-    extends: [js.configs.recommended,
+    extends: [
+      js.configs.recommended,
       // ...tseslint.configs.recommended
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.strictTypeChecked,
@@ -28,6 +30,7 @@ export default tseslint.config(
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -37,9 +40,10 @@ export default tseslint.config(
       ],
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      ...jsxA11y.configs.strict.rules,
       // custom
       "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-confusing-void-expression": "off"
+      "@typescript-eslint/no-confusing-void-expression": "off",
     },
   },
 )
