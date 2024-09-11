032b48b9f66ad63c621b506497e4d81b632f894a
notend <xyqinsong@163.com>
2024-09-11 22:03:38 +0800

    build: add vite-plugin-checker and  vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  server: {
+    open: true,
+    port: 3010,
+  },
+  preview: {
+    port: 3011,
+  },
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        tsconfigPath: 'tsconfig.app.json',
+      },
+      // eslint: { lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"' },
+    }),
+    viteTsconfigPaths(),
+  ],
 })
