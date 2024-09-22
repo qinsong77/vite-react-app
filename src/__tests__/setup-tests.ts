7c534cc848637336d35cb207ce1f9fd4d2e15103
notend <xyqinsong@163.com>
2024-09-23 00:44:24 +0800

    fix: profile test failed

--- src/__tests__/setup-tests.ts
+++ src/__tests__/setup-tests.ts
@@ -1,9 +1,12 @@
 import '@testing-library/jest-dom/vitest'
-import { cleanup } from '@testing-library/react'
+import { cleanup, configure } from '@testing-library/react'
 import { afterAll, afterEach, beforeAll } from 'vitest'
 
 import { server } from '@/__mocks__/node'
 
+configure({
+  asyncUtilTimeout: 3 * 1000,
+})
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'error' })
 })
