c4de5db38a920866f8330ba9e797349eec6056c8
notend <xyqinsong@163.com>
2024-09-23 23:44:55 +0800

    feat: add react 19 and use hook example

--- src/pages/react-19/use-hook.tsx
+++ src/pages/react-19/use-hook.tsx
@@ -1,33 +1,82 @@
-// import { Suspense, useState } from 'react'
-//
-// import { findPetsByStatus } from '@/service/petstore/endpoint'
-// import { FindPetsByStatusStatus } from '@/service/petstore/model'
+import { Suspense, use, useState } from 'react'
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Separator } from '@/components/ui/separator'
+import { findPetsByStatus } from '@/service/petstore/endpoint'
+import { FindPetsByStatusStatus } from '@/service/petstore/model'
 
 export const UseHooksPage = () => {
   return (
     <div>
-      <h2>Use</h2>
+      <h2 className="my-3 text-lg font-semibold">use Hook example</h2>
+      <UseHookExample />
     </div>
   )
 }
 
-// const useHookExample = () => {
-//   const [findPetsByStatusPromise, setFindPetsByStatusPromise] = useState(() =>
-//     findPetsByStatus({ status: FindPetsByStatusStatus.sold })
-//   )
-//   return (
-//     <div>
-//       <h3>Find Pets By Status</h3>
-//       <Suspense fallback={<p>loading...</p>}></Suspense>
-//     </div>
-//   )
-// }
-//
-// const PetsStatusList = ({
-//   findPetsByStatusPromise,
-// }: {
-//   findPetsByStatusPromise: ReturnType<typeof findPetsByStatus>
-// }) => {
-//   const resp = use(findPetsByStatusPromise)
-//   return <div></div>
-// }
+const UseHookExample = () => {
+  const [findPetsByStatusPromise, setFindPetsByStatusPromise] = useState(() =>
+    findPetsByStatus({ status: undefined })
+  )
+  return (
+    <div>
+      <h3 className="my-2">Find Pets By Status</h3>
+      <Select
+        onValueChange={(value: FindPetsByStatusStatus) => {
+          setFindPetsByStatusPromise(findPetsByStatus({ status: value }))
+        }}
+      >
+        <SelectTrigger className="w-[230px]">
+          <SelectValue placeholder="Select Status" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Status</SelectLabel>
+            {Object.entries(FindPetsByStatusStatus).map(([key, text]) => (
+              <SelectItem value={key} key={key}>
+                {text}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Separator className="mt-4" />
+      <Suspense
+        fallback={
+          <p className="my-4 text-sm text-muted-foreground">loading...</p>
+        }
+      >
+        <PetsStatusList findPetsByStatusPromise={findPetsByStatusPromise} />
+      </Suspense>
+    </div>
+  )
+}
+
+const PetsStatusList = ({
+  findPetsByStatusPromise,
+}: {
+  findPetsByStatusPromise: ReturnType<typeof findPetsByStatus>
+}) => {
+  const resp = use(findPetsByStatusPromise)
+  return (
+    <div>
+      {resp.map(({ name, status, photoUrls }) => (
+        <ul className="my-2 gap-1 py-2" key={name}>
+          <li>name: {name}</li>
+          <li>status: {status}</li>
+          <li className="text-sm text-muted-foreground">
+            photoUrls: {photoUrls.join(',')}
+          </li>
+        </ul>
+      ))}
+    </div>
+  )
+}
